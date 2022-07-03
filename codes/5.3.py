2f31c5383e759bf7c5848d4225b26c226cb49a0d
karna-rash <abhinay.sadineni@gmail.com>
2022-07-03 21:59:02 +0530

    Scatter plot added

---
+++ codes/5.3.py
@@ -0,0 +1,15 @@
+import numpy as np
+import mpmath as mp
+import scipy 
+import matplotlib.pyplot as plt
+
+N=1000000
+randvar = np.loadtxt('mix.dat',dtype='double')
+x = range(N)
+plt.plot(x,randvar)
+plt.grid() 
+plt.xlabel('Scatter plot')
+plt.ylabel('Y')
+plt.savefig('../figs/scatter_plot.pdf')
+plt.savefig('../figs/scatter_plot.eps')
+plt.show()
